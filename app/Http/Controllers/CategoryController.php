bcae22c9b698025eb461acea486669224da75476
imranhussainlaravel <prinsekhan007@gmail.com>
2025-01-30 00:06:23 +0500

    Update CategoryController.php

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class CategoryController extends Controller
 {
     public function get_industry(){
-        print_r("working fine");
+        return response()->json(['message' => 'working fine']);
     }
 }
