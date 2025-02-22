69fc44293237a7b96603e6f9626dfead8a2307b7
imranhussainlaravel <prinsekhan007@gmail.com>
2025-02-22 16:48:00 +0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,6 @@ use App\Http\Controllers\CategoryController;
 // })->middleware('auth:sanctum');
 Route::get('/get_industry',[CategoryController::class, 'get_industry']);
 // Route::get('/get_industry', [CategoryController::class, 'get_industry'])->middleware('check.origin');
-Route::middleware('check.origin')->group(function () {
-    Route::get('/get_industry', [CategoryController::class, 'get_industry']);
-});
+// Route::middleware('check.origin')->group(function () {
+//     Route::get('/get_industry', [CategoryController::class, 'get_industry']);
+// });
