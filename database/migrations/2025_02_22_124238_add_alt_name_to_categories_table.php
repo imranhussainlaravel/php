92973f10126e6b8afc3bc8ab1455e7566853643d
imranhussainlaravel <prinsekhan007@gmail.com>
2025-02-22 17:49:48 +0500

    Update 2025_02_22_124238_add_alt_name_to_categories_table.php

--- database/migrations/2025_02_22_124238_add_alt_name_to_categories_table.php
+++ database/migrations/2025_02_22_124238_add_alt_name_to_categories_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('categories', function (Blueprint $table) {
-            $table->string('alt_name')->nullable()->after('name');
+            $table->string('alt_name')->nullable()->after('main_img');
         });
     }
 
