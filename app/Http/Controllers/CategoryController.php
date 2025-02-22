0eff2180a4bf69234498f37b46a7aa176716d4f9
imranhussainlaravel <prinsekhan007@gmail.com>
2025-02-22 18:30:03 +0500

    a

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Categories;
+use App\Models\Product;
+
 
 class CategoryController extends Controller
 {
@@ -58,4 +60,41 @@ class CategoryController extends Controller
         return response()->json($response);
 
     }
+    public function get_category_by_id(Request $request)
+{
+    // Validate the incoming request
+    $id = $request->input('id');
+
+    if (!$id) {
+        return response()->json(['message' => 'ID is required'], 400);
+    }
+
+    // Fetch category based on ID from the JSON request
+    $category = Categories::where('id', $request->id)
+        ->where('status', 'active')
+        ->select('id', 'title', 'description', 'main_img', 'alt_name' , 'header_img','nav_id')
+        ->first();
+
+    $productModel = new Product();
+    $query = $productModel->select('id', 'title', 'image_1','alt_name') // Select specific columns
+    ->where('status', 'active'); // Common condition
+
+    if ($category->nav_id == '1') {
+        $query->where('industry_id', $category->id);
+    } elseif ($category->nav_id == '2') {
+        $query->where('material_id', $category->id);
+    } elseif ($category->nav_id == '3') {
+        $query->where('style_id', $category->id);
+    }
+    
+    $products = $query->get()->toArray();
+    
+
+    return response()->json([
+        'category' => $category,
+        'products' => $products,
+        'message' => 'Category found successfully'
+    ]);
+}
+
 }
--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,8 @@ use App\Http\Controllers\CategoryController;
 // })->middleware('auth:sanctum');
 Route::get('/get_industry',[CategoryController::class, 'get_industry']);
 Route::get('/get_all_category',[CategoryController::class, 'get_all_category']);
+Route::post('/get_category_by_id', [CategoryController::class, 'get_category_by_id']);
+
 // Route::get('/get_industry', [CategoryController::class, 'get_industry'])->middleware('check.origin');
 // Route::middleware('check.origin')->group(function () {
 //     Route::get('/get_industry', [CategoryController::class, 'get_industry']);
