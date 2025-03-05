8a762e4ad220ed4a5eeba8d06089646b40675531
imranhussainlaravel <prinsekhan007@gmail.com>
2025-03-05 22:49:11 +0500

    Update Products.php

--- app/Models/Products.php
+++ app/Models/Products.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Products extends Model
 {
-    //
+    protected $table = 'products'; // Ensure correct table name
+
+    protected $fillable = [
+        'title',
+        'title_2',
+        'alt_name',
+        'description',
+        'description_2',
+        'faqs',
+        'content',
+        'industry_id',
+        'material_id',
+        'style_id',
+        'image_1',
+        'image_2',
+        'image_3',
+        'image_4',
+        'image_5',
+        'status'
+    ];
 }
