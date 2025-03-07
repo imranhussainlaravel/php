eb51b46e97859f84b5108aa14fd262be546973a3
imranhussainlaravel <prinsekhan007@gmail.com>
2025-03-07 22:04:11 +0500

    Update Blog.php

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Blog extends Model
 {
-    //
+    
+    protected $fillable = [
+        'title',  // Add this
+        'image',
+        'content',
+        'sorting',
+        'status'
+    ];
 }
