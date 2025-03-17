3cde8e703c127ccf07d2b9a842e5e9bbe41a2e1e
imranhussainlaravel <prinsekhan007@gmail.com>
2025-03-17 21:36:14 +0500

    Update Request.php

--- app/Models/Request.php
+++ app/Models/Request.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Request extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name', 'email', 'phone','product_name', 'quantity', 'measurements','description'];
 }
