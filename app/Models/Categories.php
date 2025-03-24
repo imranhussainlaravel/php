ce8fded0f6b04b76333cb47a0f93e40f135ed09c
imranhussainlaravel <prinsekhan007@gmail.com>
2025-03-24 20:58:29 +0500

    Update Categories.php

--- app/Models/Categories.php
+++ app/Models/Categories.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes; // Import SoftDeletes
 
 class Categories extends Model
 {
-    //
+    use SoftDeletes; // Enable soft deletes
+
+    protected $dates = ['deleted_at']; // Ensure deleted_at is treated as a date
 }
